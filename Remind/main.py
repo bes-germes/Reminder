8817957abe4d531d6efca8f3a666987be798b240
bes-germes <55259601+bes-germes@users.noreply.github.com>
2020-06-03 05:44:35 +0300

    Update main.py

--- Remind/main.py
+++ Remind/main.py
@@ -1,31 +1,26 @@
 import json
 import datetime
-import time
 import requests
 import configparser
 import logging
 
 logging.basicConfig(filename='log.log', level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
 
 settings = configparser.ConfigParser()
 settings.sections()
 settings.read('settings.ini')
 headers = json.loads((settings['DEFAULT']['headers']))
 url = settings['DEFAULT']['url']
-print(headers)
 
 
 def mk_dir(m_id, id_of_users, mk_dir_task):
-    r = requests.post("http://" + url + "/api/v4/channels/direct",
+    r = requests.post(f"http://{url}/api/v4/channels/direct",
                       json=[
                           id_of_users,
                           m_id
                       ], headers=headers)
     channel_id = (r.json())['id']
-    r = requests.post("http://" + url + "/api/v4/posts",
+    r = requests.post(f"http://{url}/api/v4/posts",
                       json={
                           "channel_id": channel_id,
                           "message": "You've got this message as a member" + mk_dir_task[
@@ -34,15 +29,14 @@ def mk_dir(m_id, id_of_users, mk_dir_task):
 
 
 def send(rule, team_name):
-    r = requests.get("http://" + url + "/api/v4/teams/name/" + team_name,
-                     headers=headers)  
+    r = requests.get(f"http://{url}/api/v4/teams/name/" + team_name,
+                     headers=headers)
     team_id = r.json()["id"]
-    r = requests.get("http://" + url + "/api/v4/teams/" + team_id + "/members", headers=headers)
+    r = requests.get(f"http://{url}/api/v4/teams/{team_id}/members", headers=headers)
     team_members = (r.json())
-    r = requests.get("http://" + url + "/api/v4/users", headers=headers)
+    r = requests.get(f"http://{url}/api/v4/users", headers=headers)
     users_json = r.json()
-    print(rule)
-    r = requests.get("http://" + url + "/api/v4/users/me", headers=headers)
+    r = requests.get(f"http://{url}/api/v4/users/me", headers=headers)
     my_id = (r.json())['id']
     group_users = list(filter(lambda user: user['id'] in list((d['user_id'] for d in team_members)), users_json))
 
@@ -75,8 +69,10 @@ def send(rule, team_name):
 
 
 remember_minute = -1
-json_file = open("rules.json", 'r')
-data = json.load(json_file)
+remember_hour = -1
+remember_day = -1
+with open("rules.json", 'r') as json_file:
+    data = json.load(json_file)
 json_file.close()
 for team in data:
     for task in team['rules']:
@@ -86,18 +82,20 @@ while True:
     now = datetime.datetime.now()
     for team in data:
         for task in team['rules']:
-            task_time = datetime.datetime.strptime(  # try to delta
+            task_time = datetime.datetime.strptime( 
                 task['when']['time'], '%d-%m-%Y_%H:%M:%S')
             days_data = task['when']['repeat']['days']
             hours_data = task['when']['repeat']['hours']
             minutes_data = task['when']['repeat']['minutes']
             if task['when']['principe'] == "period":
                 period = now - task_time
-                if remember_minute != now.minute:
+                if remember_minute != now.minute and remember_day != now.day and remember_hour != now.hour:
                     if period.total_seconds() // 60 % (
                             task['when']['repeat']['days'] * 60 * 24 + task['when']['repeat']['hours'] * 60 +
                             task['when']['repeat']['minutes']) == 0 and now >= task_time:
                         remember_minute = now.minute
+                        remember_hour = now.hour
+                        remember_day = now.day
                         send(task, team['team'])
 
             else:
@@ -109,3 +107,4 @@ while True:
                                                    minutes=minutes_data)
                     remember_minute = now.minute
                     send(task, team['team'])
+
